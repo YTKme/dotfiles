cfd431d29af6a5915b33023ed8ff925582693508
Yan Kuang <YTKme@Outlook.com>
2023-06-21 18:48:39 -0700

    Add Windows PowerShell configuration

---
+++ windows/base/WindowsPowerShell/Profile.ps1
@@ -0,0 +1,14 @@
+#
+# Filename: Profile.ps1
+#
+
+$ErrorActionPreference = 'Stop'
+
+$Filename = 'Profile.ps1'
+
+Write-Verbose "Entering $Filename"
+
+# Write-Host 'Execute `Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process`'
+# Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+
+Write-Verbose "Leaving $Filename"
