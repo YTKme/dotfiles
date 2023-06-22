4503e96f1ccbf3484bed75fdc2102a56d84268ba
Yan Kuang <YTKme@Outlook.com>
2023-06-21 19:08:12 -0700

    Update Windows PowerShell Profile script

--- windows/base/WindowsPowerShell/Profile.ps1
+++ windows/base/WindowsPowerShell/Profile.ps1
@@ -7,8 +7,10 @@ $ErrorActionPreference = 'Stop'
 $Filename = 'Profile.ps1'
 
 Write-Verbose "Entering $Filename"
+Write-Host "Start Profile" -ForegroundColor Cyan
 
 # Write-Host 'Execute `Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process`'
 # Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 
+Write-Host "End Profile" -ForegroundColor Cyan
 Write-Verbose "Leaving $Filename"
